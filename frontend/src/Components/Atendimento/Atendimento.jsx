75cce1f9240faa20c6eaa7ae276291a5aa208947
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2026-07-15 17:36:59 -0300

    feat(atendimento): developing atendimento.jsx

--- frontend/src/Components/Atendimento/Atendimento.jsx
+++ frontend/src/Components/Atendimento/Atendimento.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import * as A from './Styles'
+import location from '../../Assets/img/location.png'
+import checked from '../../Assets/img/checked.png'
+import clock from '../../Assets/img/clock.png'
 
 export const Atendimento = () => {
   return (
@@ -12,6 +15,87 @@ export const Atendimento = () => {
         <A.descDiv>
             <p>Escolha a modalidade que melhor se adapta à sua rotina e necessidades. Em ambas as opções, você terá o mesmo cuidado atencioso e profissional.</p>
         </A.descDiv>
+
+        <A.cardDiv>
+          <A.card>
+            <A.cardHeader>
+              <span>
+                  <img src={location} alt="Icone de localização" />
+              </span>
+
+              <h2>Atendimento Presencial</h2>
+              <p>Consultas realizadas na Pronto Clínica Luna, em Pouso Alegre - MG, em um ambiente acolhedor e confortável.</p>
+            </A.cardHeader>
+
+            <A.cardBody>
+              <span>
+                <img src={checked} alt="Icone de check" />
+                <p>Ambiente reservado e confortável</p>
+              </span>
+
+              <span>
+                <img src={checked} alt="Icone de check" />
+                <p>Equipamentos modernos</p>
+              </span>
+
+              <span>
+                <img src={checked} alt="Icone de check" />
+                <p>Fácil acesso e estacionamento</p>
+              </span>
+
+              <span>
+                <img src={checked} alt="Icone de check" />
+                <p>Atendimento personalizado</p>
+              </span>
+            </A.cardBody>
+          </A.card>
+
+          <A.card>
+            <A.cardHeader>
+              <span>
+                  <img src={location} alt="Icone de localização" />
+              </span>
+
+              <h2>Consulta Online</h2>
+              <p>Atendimento por telemedicina, oferecendo praticidade e flexibilidade sem comprometer a qualidade do cuidado.</p>
+            </A.cardHeader>
+
+            <A.cardBody>
+              <span>
+                <img src={checked} alt="Icone de check" />
+                <p>Conforto do seu lar</p>
+              </span>
+
+              <span>
+                <img src={checked} alt="Icone de check" />
+                <p>Horários flexíveis</p>
+              </span>
+
+              <span>
+                <img src={checked} alt="Icone de check" />
+                <p>Mesma qualidade de atendimento</p>
+              </span>
+              
+              <span>
+                <img src={checked} alt="Icone de check" />
+                <p>Plataforma segura e confidencial</p>
+              </span>
+            </A.cardBody>
+          </A.card>
+
+          <A.card>
+            <A.cardHeader className='cardClock'>
+              <span>
+                <img src={clock} alt="Icone de Relógio" />
+              </span>
+
+              <h2>Horários Flexíveis</h2>
+              <p>Oferecemos horários adaptados à sua rotina, incluindo opções durante a semana e aos sábados. Entre em contato para verificar disponibilidade e agendar sua consulta.</p>
+
+              <button>Consultar Horários</button>
+            </A.cardHeader>
+          </A.card>
+        </A.cardDiv>
     </A.main>
   )
 }
--- frontend/src/Components/Atendimento/Styles.jsx
+++ frontend/src/Components/Atendimento/Styles.jsx
@@ -5,7 +5,7 @@ export const main = styled.main`
     padding: 30px 0px 30px 0px;
     display: flex;
     flex-direction: column;
-    gap: 5px;
+    gap: 15px;
 `
 
 export const titleDiv = styled.div`
@@ -31,5 +31,95 @@ export const descDiv = styled.div`
         padding: 0px 30px 0px 30px;
         font-size: 14px;
         color: #7A838F;
+    }
+`
+
+export const cardDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+`
+
+export const card = styled.div`
+    width: 90%;
+
+    .cardClock {
+        background-color: #F2F9FB;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+        border-radius: 15px;
+
+        button {
+            width: 100%;
+            margin-top: 10px;
+            height: 40px;
+            background-color: #548290;
+            color: #fff;
+            border: none;
+            border-radius: 10px;
+        }
+
+        h2 {
+            color: #000;
+        }
+
+        p {
+            color: #7A838F;
+        }
+    }
+`
+
+export const cardHeader = styled.div`
+    background-color: #437280;
+    height: 100%;
+    padding: 30px 30px 30px 30px;
+    border-radius: 15px 15px 0px 0px;
+    color: #fff;
+
+    span {
+        background-color: #548290;
+        height: 60px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 60px;
+        border-radius: 15px;
+        
+        img {
+            width: 30px;
+        }
+    }
+
+    h2 {
+        margin-top: 15px;
+    }
+
+    p {
+        margin-top: 3px;
+        font-size: 14px;
+        font-weight: 300;
+    }
+`
+
+export const cardBody = styled.div`
+    background-color: #fff;
+    padding: 30px 30px 30px 30px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    border-radius: 0 0 15px 15px;
+
+    span {
+        display: flex;
+        justify-content: start;
+        align-items: center;
+        gap: 10px;
+
+        p {
+            font-size: 14px;
+            font-weight: 300;
+        }
     }
 `
